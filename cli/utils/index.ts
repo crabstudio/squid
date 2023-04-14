a73361d9db479c124b5489f5afb5c77f8904fcda
jabedzaman <jabedzaman2004@gmail.com>
2023-04-15 04:23:23 +0530

    (feat) add dependency installer

--- cli/utils/index.ts
+++ cli/utils/index.ts
@@ -1,5 +1,7 @@
 import { welcome } from "./welcome";
 import { nextSteps } from "./nextSteps";
 import { exitHandler } from "./errhandler";
+import { isInstallDependencies  } from "./installDependencies";
+import { installDependencies } from "./installDependencies";
 
-export { welcome, nextSteps, exitHandler };
+export { welcome, nextSteps, exitHandler,isInstallDependencies, installDependencies  };
---
+++ cli/utils/installDependencies.ts
@@ -0,0 +1,60 @@
+import chalk from "chalk";
+import { exec } from "child_process";
+import inquirer from "inquirer";
+import ora from "ora";
+
+export const isInstallDependencies = async () => {
+  const { installDependencies } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "installDependencies",
+      message: "Install dependencies?",
+      default: true,
+    },
+  ]);
+  if (installDependencies) {
+    return true;
+  }
+  return false;
+};
+
+export const installDependencies = async (
+  projectName: string,
+  packageManager: string
+) => {
+  const spinner = ora("Installing dependencies...\n").start();
+  exec(
+    packageManager + " install",
+    { cwd: process.cwd() + "/" + projectName },
+    (error, stdout, stderr) => {
+      if (error) {
+        spinner.fail(
+          chalk.red(
+            "Failed to install dependencies, please install them manually  \n"
+          )
+        );
+        console.log(error);
+        console.log(chalk.blue("Next Steps:\n"));
+        console.log(chalk.blue("\t cd " + projectName));
+        console.log(chalk.blue("\t " + packageManager + " install"));
+        console.log(chalk.blue("\t " + packageManager + " run dev"));
+        return;
+      }
+      if (stderr) {
+        spinner.fail(
+          chalk.red(
+            "Failed to install dependencies, please install them manually  \n"
+          )
+        );
+        console.log(stderr);
+        return;
+      }
+      spinner.succeed(chalk.green("Dependencies installed successfully\n"));
+      console.log(chalk.blue("Next Steps:\n"));
+      console.log(chalk.blue("\t cd " + projectName));
+      console.log(chalk.blue("\t " + packageManager + " install"));
+      console.log(chalk.blue("\t " + packageManager + " run dev"));
+      console.log(chalk.blue("\n Happy Coding!\n"));
+    }
+  );
+};
